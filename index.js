05c0c38bd2d14a509bbce9bec4de6ad59cf84ff3
Sergiybas <163581263+Sergiybas@users.noreply.github.com>
2024-04-30 18:43:36 +0300

    homework1-6

---
+++ index.js
@@ -0,0 +1,43 @@
+// Завдання 1.....
+
+// const productName = 'Droid';
+// const pricePerItem = 2000;
+
+// ..............................................
+// Завдання 2.....
+
+// let productName = "Droid";
+// let pricePerlItem = 2000;
+// productName = "Repair droid"
+// pricePerlItem = 3500;
+// console.log(productName);
+// console.log(pricePerlItem);
+
+// Завдання3.......................................
+const topSpeed = 160;
+const distance = 617.54;
+const login = "mango935";
+const isOnline = true;
+const isFdmin = false;
+
+// Завдання4.....................................
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = ` ${pricePerItem * orderedQuantity}`
+// console.log(totalPrice);
+
+// Завдання5.................................
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = ` "You picked ${productName}, price per item is ${pricePerItem} credits " `;
+// console.log(message);
+
+// Завдання6....................................
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = `${pricePerDroid*orderedQuantity+deliveryFee}`;
+const message = `"You ordered droids worth ${totalPrice} credits. Delivery( ${deliveryFee} credits) is included in total price "`;
+console.log(message);
+
+// Завдання7...........././././././././././////////////
